c3ad182fde1149fb68711166edfc387af4622d8f
Cirstescu Andrei <andrei.cirstescu@stud.acs.upb.ro>
2023-05-24 14:07:10 +0300

    Changed main.c for testing

--- src/main.c
+++ src/main.c
@@ -1,63 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "../include/matrix.h"
 #include "../include/list.h"
-
-int isNameValid(char *name) {
-    char validChars[] = "abcdefghijklmnopqrstuvwxyz"
-                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                        "0123456789-_";
-
-    for (u_int i = 0; i < strlen(name); ++i) {
-        if (!strchr(validChars, name[i])) {
-            printf("Name can contain only letters and numbers!\n");
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-Matrix* readMatrix() {
-    Matrix *newMatrix = malloc(sizeof(Matrix));
-    char tempBuff[256];
-
-    do {
-        printf("Enter a name for your matrix: ");
-        scanf("%s", tempBuff);
-    } while (isNameValid(tempBuff) == 0);
-
-    newMatrix->name = malloc((strlen(tempBuff) + 1) * sizeof(char));
-    strcpy(newMatrix->name, tempBuff);
-
-    printf("Enter the dimensions of the matrix you want to read:\n");
-    printf("Rows: ");
-    scanf("%d", &newMatrix->rows);
-    printf("Cols: ");
-    scanf("%d", &newMatrix->cols);
-
-    newMatrix->elems = malloc(newMatrix->rows * sizeof(double*));
-    newMatrix->elems[0] = malloc(newMatrix->rows * newMatrix->cols * sizeof(double));
-    for (int i = 1; i < newMatrix->rows; ++i) {
-        newMatrix->elems[i] = newMatrix->elems[0] + i * newMatrix->cols;
-    }
-
-    printf("Enter the elements of your matrix");
-
-    for (int i = 0; i < newMatrix->rows; ++i) {
-        for (int j = 0; j < newMatrix->cols; ++j) {
-            scanf("%lf", &newMatrix->elems[i][j]);
-        }
-    }
-
-    return newMatrix;
-}
+#include "../include/matrix.h"
 
 int main() {
     MatrixList *list = initMatList();
+
     Matrix *mat = readMatrix();
     insertMatrix(list, mat);
+
+    Matrix *mat2 = readMatrix();
+    insertMatrix(list, mat2);
 
     for (int i = 0; i < mat->rows; ++i) {
         for (int j = 0; j < mat->cols; ++j) {
@@ -66,7 +19,9 @@ int main() {
         printf("\n");
     }
 
-    freeMatList(list);
+    printMatrixList(list);
 
-	return 0;
+    freeMatList(list, freeMatrix);
+
+    return 0;
 }
