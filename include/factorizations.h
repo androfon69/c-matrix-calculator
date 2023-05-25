8e426e3d6d54d2540af25a735d9788ff6c132d07
Borromeu <adelin_andrei.fulop@stud.acs.upb.ro>
2023-05-25 21:58:27 +0300

    header de factorizari

---
+++ include/factorizations.h
@@ -0,0 +1,8 @@
+//
+// Created by adelin on 25.05.2023.
+//
+
+#ifndef C_MATRIX_CALCULATOR_FACTORIZATIONS_H
+#define C_MATRIX_CALCULATOR_FACTORIZATIONS_H
+MatrixList *crout(MatrixList *list,Matrix *matrice ,char *nume_matrice);
+#endif //C_MATRIX_CALCULATOR_FACTORIZATIONS_H
